584c9355b57a502f5ac1d40e3364eca4daf460bb
Wes Schierenbeck <wschierenbeck@gmail.com>
2022-02-11 17:56:16 -0500

    anagram generator successfully built

--- set_spawner.rb
+++ set_spawner.rb
@@ -1,61 +1,242 @@
 require 'pry'
 
-def spawnNumberSet()
-  bigs = [100, 75, 50, 25]
-  smalls = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-  big_options = [
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    3,
-    3,
-  ]
+@bigs = [100, 75, 50, 25]
+@smalls = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+@big_options = [
+  1,
+  1,
+  1,
+  1,
+  1,
+  1,
+  1,
+  1,
+  1,
+  1,
+  1,
+  1,
+  1,
+  1,
+  1,
+  2,
+  2,
+  2,
+  2,
+  2,
+  2,
+  2,
+  2,
+  2,
+  2,
+  3,
+  3,
+]
+@vowels = %w[
+  a
+  a
+  a
+  a
+  a
+  a
+  a
+  a
+  a
+  a
+  a
+  a
+  a
+  a
+  a
+  e
+  e
+  e
+  e
+  e
+  e
+  e
+  e
+  e
+  e
+  e
+  e
+  e
+  e
+  e
+  e
+  e
+  e
+  e
+  e
+  e
+  i
+  i
+  i
+  i
+  i
+  i
+  i
+  i
+  i
+  i
+  i
+  i
+  i
+  o
+  o
+  o
+  o
+  o
+  o
+  o
+  o
+  o
+  o
+  o
+  o
+  o
+  u
+  u
+  u
+  u
+  u
+]
+@consonants = %w[
+  b
+  b
+  c
+  c
+  c
+  d
+  d
+  d
+  d
+  d
+  d
+  f
+  f
+  g
+  g
+  g
+  h
+  h
+  j
+  k
+  l
+  l
+  l
+  l
+  l
+  m
+  m
+  m
+  m
+  n
+  n
+  n
+  n
+  n
+  n
+  n
+  n
+  p
+  p
+  p
+  p
+  q
+  r
+  r
+  r
+  r
+  r
+  r
+  r
+  r
+  r
+  s
+  s
+  s
+  s
+  s
+  s
+  s
+  s
+  s
+  t
+  t
+  t
+  t
+  t
+  t
+  t
+  t
+  t
+  v
+  w
+  x
+  y
+  z
+]
+@vowel_options = [
+  3,
+  3,
+  3,
+  3,
+  3,
+  4,
+  4,
+  4,
+  4,
+  4,
+  4,
+  4,
+  4,
+  4,
+  4,
+  4,
+  4,
+  4,
+  4,
+  4,
+  4,
+  4,
+  4,
+  4,
+  4,
+  4,
+  4,
+  4,
+  4,
+  5,
+  5,
+]
+
+################## NUMBERS GAME ###################
+
+# spawns set of six numbers, some large and some small, reflective of show's ratio
+def spawn_number_set()
   set = []
-  number_of_bigs = big_options.sample(1)[0]
+  number_of_bigs = @big_options.sample(1)[0]
   number_of_smalls = (6 - number_of_bigs)
   bcounter = 1
   scounter = 1
   while bcounter <= number_of_bigs
-    given_big = bigs.sample(1)[0]
+    given_big = @bigs.sample(1)[0]
     set.push(given_big)
     bcounter = bcounter + 1
   end
   while scounter <= number_of_smalls
-    given_small = smalls.sample(1)[0]
+    given_small = @smalls.sample(1)[0]
     set.push(given_small)
     scounter = scounter + 1
   end
   return set
 end
 
-s1 = spawnNumberSet
+s1 = spawn_number_set
 
-# set of numbers
-# grab one as first number
-# from there: either grab one and an operand, then perform that operation on our living total
-# OR, grab two, operand them, then use their result on our living total
+# Grab one random number from created set to start, then either perform A or B
+# A: grab another number and an operand, then perform that operation on our total
+# B:, grab two numbers and an operand, perform operand on those numbers, then add result to our array of numbers to be used in a future operation.
 # go until array is empty
 
 def smart_create_target(set)
@@ -171,154 +352,9 @@ def smart_create_target(set)
 end
 
 smart_create_target(s1)
-###########################################
 
-@word_db = []
-
-def add_word(word)
-  if word.include?('1')
-    word.delete('1')
-  elsif word.include?('2')
-    word.delete('2')
-  elsif word.include?('3')
-    word.delete('3')
-  end
-  @word_db.push(word) if clean_word(word)
-end
-
-def grab_contents(file)
-  opened_file = File.open(file)
-  file_contents = opened_file.readlines.map(&:chomp)
-  opened_file.close
-  return file_contents
-end
-
-# pp(grab_contents('./o_e_d.txt'))
-
-def grab_first_word(file)
-  dictionary = grab_contents('./o_e_d.txt')
-  dictionary.each do |given_line|
-    if given_line.length > 0
-      first_white = given_line.index(' ')
-      word = given_line.slice(0..first_white)
-      clean = word.chop
-      last_character = clean[clean.length - 1]
-      if clean != ''
-        while clean[last_character] == ' ' || clean[last_character] == '1' ||
-                clean[last_character] == '2'
-          clean = clean.chop
-          last_character = clean[clean.length - 1]
-        end
-      end
-      add_word(clean) if clean.length > 1 && clean_word(clean) == true
-    end
-  end
-end
-
-def clean_word(word)
-  comparison = word.downcase
-  if comparison.include?('-')
-    return false
-  elsif comparison.include?('.')
-    return false
-  elsif comparison.include?("'")
-    return false
-  elsif comparison.include?('/')
-    return false
-  elsif comparison.include?('[')
-    return false
-  elsif comparison.include?(']')
-    return false
-  elsif comparison.include?('*')
-    return false
-  elsif comparison.include?('(')
-    return false
-  elsif comparison.include?(')')
-    return false
-  elsif comparison.include?('a') == false &&
-        comparison.include?('e') == false &&
-        comparison.include?('i') == false &&
-        comparison.include?('u') == false &&
-        comparison.include?('o') == false && comparison.include?('y') == false
-    return false
-  elsif comparison.length <= 1
-    return false
-  else
-    return true
-  end
-end
-
-def parse_line(dic_entry)
-  if dic_entry.length > 0
-    entry = dic_entry.split(' ')
-    word = entry.slice!(0, 1)[0]
-    if word
-      if (clean_word(word) == true)
-        clean = word.chop
-        add_word(clean)
-      else
-        add_word(word)
-      end
-    end
-    if entry[0] == 'n.' || entry[0] == 'v.'
-      if entry.include?('(pl.') == false
-        plural = word + 's'
-        add_word(plural)
-      end
-    end
-    entry.delete_at(0)
-    entry.each_with_index do |text, index|
-      if text === 'n.'
-        noun = entry[index - 1]
-        add_word(noun) if (clean_word(noun) == true)
-      elsif text === 'adj.'
-        adjective = entry[index - 1]
-        add_word(adjective) if (clean_word(adjective) == true)
-      elsif text === 'adv.'
-        adverb = entry[index - 1]
-        add_word(adverb) if (clean_word(adverb) == true)
-      elsif text === 'v.'
-        verb = entry[index - 1]
-        add_word(verb) if (clean_word(verb) == true)
-      elsif text === '(pl.'
-        handle_plurals(entry, word, text, index)
-      end
-    end
-  end
-end
-
-def handle_plurals(entry, word, text, index)
-  if entry[index + 1] == '-s)'
-    plural = word + 's'
-    add_word(plural)
-  elsif entry[index + 1] == '-ies)'
-    counter = word.length - 1
-    while counter >= 0
-      if word[counter] == 'y'
-        base = word.slice(0, counter - 1)
-        plural = base + 'ies'
-        add_word(plural)
-        break
-      else
-        counter = counter - 1
-      end
-    end
-  end
-end
-
-def grab_first_word_easy(file)
-  dictionary = grab_contents(file)
-  dictionary.each { |given_line| parse_line(given_line) }
-  pp(@word_db)
-end
-
-# grab_first_word('./o_e_d.txt')
-# pp(clean_word('leg-of-mutton'))
-
-# grab_first_word_easy('./o_e_d.txt')
-
-##########
-#Dictionary Parser, attempt 2:
+################# DICTIONARY ######################
+# Parsing OED txt file for a list of words:
 
 @dictionary = []
 
@@ -359,8 +395,7 @@ def parse_dictionary(file)
     end
   end
   dictionary = @dictionary.uniq
-  pp(dictionary.sort)
-  return dictionary
+  return dictionary.sort
 end
 
 def grab_word(entry)
@@ -479,47 +514,78 @@ def valid_word(word)
 end
 
 parse_dictionary('./oxford_english_dictionary.txt')
-###########################################################################################
-# def create_target(set)
-#   operands = %w[+ - * /]
-#   requirements = [3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6]
-#   steps = requirements.sample(1)[0]
-#   working_numbers = set.sample(steps)
-#   pp(working_numbers)
-#   solution = []
-#   scounter = 1
-#   total = working_numbers.slice!(0, 1)[0]
-#   working_numbers.each_with_index do |given_number, index|
-#     operand = operands.sample(1)[0]
-#     if operand == '+'
-#       solution.push("#{total} + #{given_number} = #{total + given_number}")
-#       total = total + given_number
-#     elsif operand == '-'
-#       if total > given_number
-#         solution.push("#{total} - #{given_number} = #{total - given_number}")
-#         total = total - given_number
-#       else
-#         working_numbers.insert(index + 1, given_number)
-#         next
-#       end
-#     elsif operand == '*'
-#       solution.push("#{total} * #{given_number} =  #{total * given_number}")
-#       total = total * given_number
-#     elsif operand == '/'
-#       if total % given_number == 0 && given_number !== 1
-#         solution.push("#{total} / #{given_number} = #{total / given_number}")
-#         total = total / given_number
-#       else
-#         working_numbers.insert(index + 1, given_number)
-#         next
-#       end
-#     end
-#   end
-#   if total >= 100 && total <= 999
-#     pp(solution)
-#     pp(total)
-#     return solution, total
-#   else
-#     create_target(set)
-#   end
-# end
+
+#######################################
+
+def spawn_letter_set()
+  set = []
+  number_of_vowels = @vowel_options.sample(1)[0]
+  number_of_consonants = 9 - number_of_vowels
+  vcounter = 1
+  ccounter = 1
+  while vcounter <= number_of_vowels
+    given_vowel = @vowels.sample(1)[0]
+    set.push(given_vowel)
+    vcounter = vcounter + 1
+  end
+  while ccounter <= number_of_consonants
+    given_cons = @consonants.sample(1)[0]
+    set.push(given_cons)
+    ccounter = ccounter + 1
+  end
+  return set
+end
+
+s2 = spawn_letter_set
+
+def anagram_finder(set)
+  pp("letters: #{set}")
+  twos = []
+  threes = []
+  fours = []
+  fives = []
+  sixes = []
+  sevens = []
+  eights = []
+  nines = []
+  @dictionary.each do |word|
+    split = word.split('')
+    if (split - set).empty?
+      if word.length == 2
+        twos.push(word)
+      elsif word.length == 3
+        threes.push(word)
+      elsif word.length == 4
+        fours.push(word)
+      elsif word.length == 5
+        fives.push(word)
+      elsif word.length == 6
+        sixes.push(word)
+      elsif word.length == 7
+        sevens.push(word)
+      elsif word.length == 8
+        eights.push(word)
+      elsif word.length == 9
+        nines.push(word)
+      end
+    end
+  end
+  pp('2s:')
+  pp(twos.uniq)
+  pp('3s:')
+  pp(threes.uniq)
+  pp('4s:')
+  pp(fours.uniq)
+  pp('5s:')
+  pp(fives.uniq)
+  pp('6s:')
+  pp(sixes.uniq)
+  pp('7s:')
+  pp(sevens.uniq)
+  pp('8s:')
+  pp(eights.uniq)
+  pp('9s:')
+  pp(nines.uniq)
+end
+
+anagram_finder(s2)
