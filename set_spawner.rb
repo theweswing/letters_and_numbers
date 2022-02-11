271719e37c91f0a3f2db8b5a512c8640fc4c6415
Wes Schierenbeck <wschierenbeck@gmail.com>
2022-02-11 16:01:46 -0500

    refactored dictionary

--- set_spawner.rb
+++ set_spawner.rb
@@ -176,7 +176,14 @@ smart_create_target(s1)
 @word_db = []
 
 def add_word(word)
-  @word_db.push(word)
+  if word.include?('1')
+    word.delete('1')
+  elsif word.include?('2')
+    word.delete('2')
+  elsif word.include?('3')
+    word.delete('3')
+  end
+  @word_db.push(word) if clean_word(word)
 end
 
 def grab_contents(file)
@@ -247,12 +254,10 @@ def parse_line(dic_entry)
     word = entry.slice!(0, 1)[0]
     if word
       if (clean_word(word) == true)
-        if word.include?('1') || word.include?('2')
-          clean = word.chop
-          add_word(clean)
-        else
-          add_word(word)
-        end
+        clean = word.chop
+        add_word(clean)
+      else
+        add_word(word)
       end
     end
     if entry[0] == 'n.' || entry[0] == 'v.'
@@ -310,8 +315,170 @@ end
 # grab_first_word('./o_e_d.txt')
 # pp(clean_word('leg-of-mutton'))
 
-grab_first_word_easy('./o_e_d.txt')
+# grab_first_word_easy('./o_e_d.txt')
 
+##########
+#Dictionary Parser, attempt 2:
+
+@dictionary = []
+
+def read_file(file)
+  opened_file = File.open(file)
+  file_contents = opened_file.readlines.map(&:chomp)
+  opened_file.close
+  return file_contents
+end
+
+def add_to_dictionary(word)
+  clean = remove_numbers(word)
+  @dictionary.push(clean.downcase)
+  return clean
+end
+
+def remove_numbers(word)
+  if word.include?('1')
+    word.delete!('1')
+    return word
+  elsif word.include?('2')
+    word.delete!('2')
+    return word
+  elsif word.include?('é')
+    word = word.gsub!('é', 'e')
+    return word
+  else
+    return word
+  end
+end
+
+def parse_dictionary(file)
+  dic = read_file(file)
+  dic.each do |given_entry|
+    if given_entry.length > 1
+      word = grab_word(given_entry)
+      grab_forms(given_entry, word)
+    end
+  end
+  dictionary = @dictionary.uniq
+  pp(dictionary.sort)
+  return dictionary
+end
+
+def grab_word(entry)
+  contents = entry.split(' ')
+  if contents[1] != 'abbr.' && valid_word(contents[0]) == true
+    word = add_to_dictionary(contents[0])
+    return word
+  end
+end
+
+def grab_forms(entry, word)
+  contents = entry.split(' ')
+  if word
+    grab_verb_variations(contents, word)
+    grab_comparative_suffixes(contents, word)
+  end
+  contents.each_with_index do |text, index|
+    grab_alt_forms(contents, word, text, index)
+  end
+end
+
+def grab_alt_forms(contents, word, text, index)
+  if text === 'n.'
+    noun = contents[index - 1]
+    add_to_dictionary(noun) if (valid_word(noun) == true)
+  elsif text === 'adj.'
+    adjective = contents[index - 1]
+    add_to_dictionary(adjective) if (valid_word(adjective) == true)
+  elsif text === 'adv.'
+    adverb = contents[index - 1]
+    add_to_dictionary(adverb) if (valid_word(adverb) == true)
+  elsif text === 'v.'
+    verb = contents[index - 1]
+    add_to_dictionary(verb) if (valid_word(verb) == true)
+  elsif text === '(pl.'
+    grab_plural(contents, word, text, index)
+  end
+end
+
+def grab_verb_variations(entry, word)
+  if entry[1] == 'v.'
+    suffix_finder(word, 's', 'sing') if entry.include?('(-sing)')
+    suffix_finder(word, 't', 'ting') if entry.include?('(-ting)')
+  end
+end
+
+def grab_plural(entry, word, text, index)
+  if word
+    if entry[index + 1] == '-s)'
+      plural = word + 's'
+      add_to_dictionary(plural)
+    elsif entry[index + 1] == 'Same' && entry[index + 3] == '-s)'
+      plural = word + 's'
+      add_to_dictionary(plural)
+    elsif entry[index + 1] == '-cuses)'
+      plural = word + 'es'
+      add_to_dictionary(plural)
+    elsif entry[index + 1] == '-ies)'
+      suffix_finder(word, 'y', 'ies')
+    end
+  end
+end
+
+def grab_comparative_suffixes(entry, word)
+  suffix_finder(word, 'y', 'ier') if entry.include?('(-ier,')
+  if entry.include?('-iest)') || entry.include?('-iest)')
+    suffix_finder(word, 'y', 'iest')
+  end
+end
+
+def suffix_finder(word, find, replace)
+  counter = word.length - 1
+  while counter >= 0
+    if word[counter] == find
+      new_word = word.slice(0, counter) + replace
+      add_to_dictionary(new_word)
+      return new_word
+      break
+    else
+      counter = counter - 1
+    end
+  end
+end
+
+def valid_word(word)
+  comparison = word.downcase
+  if comparison.include?('-')
+    return false
+  elsif comparison.include?('.')
+    return false
+  elsif comparison.include?("'")
+    return false
+  elsif comparison.include?('/')
+    return false
+  elsif comparison.include?('[')
+    return false
+  elsif comparison.include?(']')
+    return false
+  elsif comparison.include?('*')
+    return false
+  elsif comparison.include?('(')
+    return false
+  elsif comparison.include?(')')
+    return false
+  elsif comparison.include?('a') == false &&
+        comparison.include?('e') == false &&
+        comparison.include?('i') == false &&
+        comparison.include?('u') == false &&
+        comparison.include?('o') == false && comparison.include?('y') == false
+    return false
+  elsif comparison.length <= 1
+    return false
+  else
+    return true
+  end
+end
+
+parse_dictionary('./oxford_english_dictionary.txt')
 ###########################################################################################
 # def create_target(set)
 #   operands = %w[+ - * /]
